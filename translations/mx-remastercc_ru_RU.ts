41b78663487f89868d2f5a2fb74063d14ec6ecd3
Adrian <adrian@mxlinux.org>
2024-02-17 12:01:50 -0500

    New build

--- translations/mx-remastercc_ru_RU.ts
+++ translations/mx-remastercc_ru_RU.ts
@@ -1,32 +1,32 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="hr">
+<TS version="2.1" language="ru_RU">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>MX Remastercc</source>
-        <translation type="unfinished"></translation>
+        <translation>MX Центр ремастеринга</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="64"/>
         <source>Persistence</source>
-        <translation type="unfinished"></translation>
+        <translation>Сохранение состояния</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="81"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;This tool allows you to rework an ISO while running Live. &lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Remaster&lt;/span&gt; allows you to change files, applications and settings with the goal of producing a new master copy for storage or distribution. &lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Persistence&lt;/span&gt; allows you to retain data, settings and installed programs when the machine gets switched off for personal use.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Этот инструмент позволяет сделать свой ISO во время живого сеанса. &lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Ремастеринг&lt;/span&gt; позволяет изменять файлы, приложения и настройки с целью получения новой копии ОС для хранения или распространения. &lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Сохранение состояния&lt;/span&gt; позволяет сохранять данные, настройки и установленные программы, при выключении компьютера, для личного использования.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="133"/>
         <source>Display help </source>
-        <translation>Prikaži pomoć</translation>
+        <translation>Показать справку</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="136"/>
         <source>Help</source>
-        <translation>Pomoć</translation>
+        <translation>Помощь</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="143"/>
@@ -36,12 +36,12 @@
     <message>
         <location filename="../mainwindow.ui" line="175"/>
         <source>About this application</source>
-        <translation>O ovoj aplikaciji</translation>
+        <translation>Об этом приложении</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="178"/>
         <source>About...</source>
-        <translation>O programu...</translation>
+        <translation>О программе...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="185"/>
@@ -51,12 +51,12 @@
     <message>
         <location filename="../mainwindow.ui" line="217"/>
         <source>Quit application</source>
-        <translation>Zatvori aplikaciju</translation>
+        <translation>Выйти из приложения</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="220"/>
         <source>Close</source>
-        <translation>Zatvori</translation>
+        <translation>Закрыть</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="227"/>
@@ -66,78 +66,78 @@
     <message>
         <location filename="../mainwindow.ui" line="264"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Basic operations&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Основные операции&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="274"/>
         <source>Remaster</source>
-        <translation type="unfinished"></translation>
+        <translation>Ремастер</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="301"/>
         <source>Save root persistence</source>
-        <translation type="unfinished"></translation>
+        <translation>Сохранить состояние корневой папки</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="315"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Optional operations&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Необязательные операции&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="325"/>
         <source>Configure live persistence</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройка сохранения данных</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="53"/>
         <location filename="../mainwindow.cpp" line="88"/>
         <source>MX Remaster Control Center</source>
-        <translation type="unfinished"></translation>
+        <translation>Центр управления ремастерингом MX</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="66"/>
         <source>MX RemasterCC</source>
-        <translation type="unfinished"></translation>
+        <translation>MX Центр ремастеринга</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="87"/>
         <source>About MX Remaster Control Center</source>
-        <translation type="unfinished"></translation>
+        <translation>О Центре управления ремастерингом MX</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="89"/>
         <source>Version: </source>
-        <translation>Inačica:</translation>
+        <translation>Версия: </translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="90"/>
         <source>This program provides access to different remaster and persistence tools in MX Linux</source>
-        <translation type="unfinished"></translation>
+        <translation>Эта программа обеспечивает доступ к различным инструментам ремастеринга и сохранения состояния в MX Linux</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="92"/>
         <source>Copyright (c) MX Linux</source>
-        <translation>Autorska prava (c) MX Linux</translation>
+        <translation>Copyright (c) MX Linux</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="93"/>
         <source>License</source>
-        <translation>Licenca</translation>
+        <translation>Лицензия</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="94"/>
         <source>Changelog</source>
-        <translation type="unfinished"></translation>
+        <translation>Список изменений</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="95"/>
         <source>Cancel</source>
-        <translation>Otkaži</translation>
+        <translation>Отмена</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="120"/>
         <source>&amp;Close</source>
-        <translation>&amp;Zatvori</translation>
+        <translation>&amp;Закрыть</translation>
     </message>
 </context>
 </TS>
