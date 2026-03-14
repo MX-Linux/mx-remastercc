a369495a5e84a140ae5b898759bfbd2ff378c330
Adrian <adrian@mxlinux.org>
2026-03-14 00:03:56 -0400

    New build

---
+++ debs/mx-remastercc_26.03.dsc
@@ -0,0 +1,33 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
+
+Format: 3.0 (native)
+Source: mx-remastercc
+Binary: mx-remastercc
+Architecture: any
+Version: 26.03
+Maintainer: Adrian <adrian@mxlinux.org>
+Homepage: https://github.com/MX-Linux/mx-remastercc
+Standards-Version: 3.9.8
+Vcs-Git: git://github.com/MX-Linux/mx-remastercc
+Build-Depends: debhelper-compat (= 13), cmake (>= 3.16), ninja-build, qt6-base-dev, qt6-base-dev-tools, qt6-tools-dev, qt6-tools-dev-tools
+Package-List:
+ mx-remastercc deb admin optional arch=any
+Checksums-Sha1:
+ f962944ce5c40dfea334ac5298b7cfee0c2b12f5 55252 mx-remastercc_26.03.tar.xz
+Checksums-Sha256:
+ 84c85d26b1f266a773fd5a908019e97d9b612aa5edbe971c2e8a91d0f9c2e099 55252 mx-remastercc_26.03.tar.xz
+Files:
+ e7fb285366b410598326c6aaf3ed487f 55252 mx-remastercc_26.03.tar.xz
+
+-----BEGIN PGP SIGNATURE-----
+
+iQFHBAEBCgAxFiEE8ndToY6S45N+YzXncJOMeAZ57pgFAmm02/cTHGFkcmlhbkBt
+eGxpbnV4Lm9yZwAKCRBwk4x4BnnumM4CB/9xI41idhKhFKUHvWyWVvQMdx8QryJ1
+mKqtnVtBEva/Z2npN+qvRtWCOkodcTQHxeiTdqrtoRDof2H9xptbpZJeRz9Wo4qD
+gm7R4nY39D30jWxuNokDAsrlY13+aGqS2GbcOPmCM6qoRn9WpFCoKSYDm/c34orD
+XMK+NXH568PYL7DNWfdPwEdCQIMfjt4hDJdmHxMzC66IzhHgJzWxvxJH1UorTO79
+cRijlzRxXKso3giiOrMPcdqJndHt1uayLi6L8BeOhXgXCsoqRbYdbhgP1CvAFvOc
+sXstKfRLN/zp6T1KCTPkRVXliDZjULkSyjJazzUifYHxLZsegcBf+RuT
+=Wr6V
+-----END PGP SIGNATURE-----
--- translations/mx-remastercc_ka.ts
+++ translations/mx-remastercc_ka.ts
@@ -1,30 +1,30 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="fa">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="ka">
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>MX Remastercc</source>
-        <translation type="unfinished"/>
+        <translation>MX Remastercc</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="64"/>
         <source>Persistence</source>
-        <translation type="unfinished"/>
+        <translation>გამძლეობა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="81"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;This tool allows you to rework an ISO while running Live. &lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Remaster&lt;/span&gt; allows you to change files, applications and settings with the goal of producing a new master copy for storage or distribution. &lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Persistence&lt;/span&gt; allows you to retain data, settings and installed programs when the machine gets switched off for personal use.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"/>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;ეს პროგრამა საშუალებას გაძლევთ, გადაამუშაოთ ISO, როცა გაშვებული გაქვთ ცოცხალი სისტემა. &lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;გადაკეტება&lt;/span&gt; საშუალებას გაზლევთ, შეცვალოთ ფაილები, აპლიკაციები და პარამეტრები დანიშნულებით, რომ შექმნათ ახალი მთავარი ასლი შესანახად, ან გასავრცელებლად. &lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;მუდმივობა&lt;/span&gt; საშუალებას გაძლევთ, შეინარჩუნოთ მონაცემები, პარამეტრები და დაყენებული პროგრამები, როცა მანქანას გამორთავთ.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="133"/>
         <source>Display help </source>
-        <translation>نمایش راهنما</translation>
+        <translation>დახმარების ჩვენება </translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="136"/>
         <source>Help</source>
-        <translation>راهنما </translation>
+        <translation>დახმარება</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="143"/>
@@ -34,12 +34,12 @@
     <message>
         <location filename="../mainwindow.ui" line="175"/>
         <source>About this application</source>
-        <translation>درباره این نرم افزار</translation>
+        <translation>ამ აპლიკაციის შესახებ</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="178"/>
         <source>About...</source>
-        <translation>درباره...</translation>
+        <translation>შესახებ...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="185"/>
@@ -49,12 +49,12 @@
     <message>
         <location filename="../mainwindow.ui" line="217"/>
         <source>Quit application</source>
-        <translation>خروج از برنامه</translation>
+        <translation>აპლიკაციიდან გასვლა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="220"/>
         <source>Close</source>
-        <translation>بستن</translation>
+        <translation>დახურვა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="227"/>
@@ -64,78 +64,78 @@
     <message>
         <location filename="../mainwindow.ui" line="264"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Basic operations&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"/>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;ძირითადი ოპერაციები&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="274"/>
         <source>Remaster</source>
-        <translation>ﺮﺘﺴﻤﯾﺭ</translation>
+        <translation>რემასტერი</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="301"/>
         <source>Save root persistence</source>
-        <translation type="unfinished"/>
+        <translation>ძირითადი საქაღალდის მუდმივობის შენახვა</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="315"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Optional operations&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"/>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;არასავალდებულო ოპერაციები&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="325"/>
         <source>Configure live persistence</source>
-        <translation type="unfinished"/>
+        <translation>ცოცხალი სისტემის მუდმივობის მორგება</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="54"/>
         <location filename="../mainwindow.cpp" line="103"/>
         <source>MX Remaster Control Center</source>
-        <translation type="unfinished"/>
+        <translation>MX Remaster Control Center</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="82"/>
         <source>MX RemasterCC</source>
-        <translation type="unfinished"/>
+        <translation>MX RemasterCC</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="102"/>
         <source>About MX Remaster Control Center</source>
-        <translation type="unfinished"/>
+        <translation>MX Remaster Control Center-ის შესახებ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="104"/>
         <source>Version: </source>
-        <translation>نسخه:</translation>
+        <translation>ვერსია: </translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="105"/>
         <source>This program provides access to different remaster and persistence tools in MX Linux</source>
-        <translation type="unfinished"/>
+        <translation>ეს პროგრამა გაწვდით წვდომას სხვადასხვა ცვლილებისა და მუდმივობის პროგრამებთან MX Linux-ში</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="107"/>
         <source>Copyright (c) MX Linux</source>
-        <translation>Copyright (c) MX Linux</translation>
+        <translation>(c) MX Linux საავტორო ფულებები დაცულია</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="108"/>
         <source>License</source>
-        <translation>لایسنس</translation>
+        <translation>ლიცენზია</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="109"/>
         <source>Changelog</source>
-        <translation>لیست موارد بهبود یافته</translation>
+        <translation>ცვლილებების ჟურნალი</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="110"/>
         <source>Cancel</source>
-        <translation>لغو</translation>
+        <translation>გაუქმება</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="128"/>
         <source>&amp;Close</source>
-        <translation>و بستن</translation>
+        <translation>&amp;დახურვა</translation>
     </message>
 </context>
 </TS>
